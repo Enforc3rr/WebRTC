2adfab0138990e6e989effb172a8a6578f3b2190
Prateek Kumar <prateekkumartiwari01@gmail.com>
2021-06-03 23:47:39 +0530

    Implemented Socket.io

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require("express");
 const app = express();
+/*
+The require('socket.io')(http) creates a new socket.io instance attached to the http server.
+this syntax basically means that socket.io needs a http object as an argument
+It looks something like this
+module.exports = function (http) {
+  // . . .
+  return socket;
+};
+*/
+const io = require("socket.io")(3000);
+const {v4} = require("uuid");
 
+app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => {
+    res.redirect(`/${v4()}`);
+});
 
-
+app.get('/:room', (req, res) => {
+    //basically we are passing in roomId to our front-end code.
+    res.render('room', { roomId: req.params.room });
+});
+//"connection is called when we load in the page , "connection" is basically emitted and this function gets called.
+io.on("connection", socket =>{
+    console.log("connected");
+    socket.on("join-room",(roomId,userId)=>{
+        console.log(userId,roomId);
+    });
+});
 
 
 
